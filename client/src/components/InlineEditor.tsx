54dc5c459ff42f3e3e8f46571e6229210706f221
Jesús Hernández <jahl-v2@hotmail.com>
2024-10-29 20:15:04 -0600

    update the inline editor to use a ref in order to focus the element when clicked

--- client/src/components/InlineEditor.tsx
+++ client/src/components/InlineEditor.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 interface TInlineEditor {
   value: string;
   onValueChanged: (value : string) => void;
 }
 
-const InlineEditor : React.FC<TInlineEditor> = (props) => {
-  const { value, onValueChanged } = props;
+const InlineEditor : React.FC<TInlineEditor> = ({ value, onValueChanged }) => {
   const [isEditing, setIsEditing] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isEditing]);
 
   const onChange = (event : React.ChangeEvent<HTMLInputElement>) => {
     onValueChanged(event.target.value);
@@ -21,8 +27,18 @@ const InlineEditor : React.FC<TInlineEditor> = (props) => {
     <div className="flex flex-row justify-center py-2">
       {
         isEditing 
-        ? <input className="text-3xl text-center py-3 min-w-full" type='text' value={value} onChange={onChange} />
-        : <h1 className="text-3xl text-center py-3" onClick={toggleEditing}>{value}</h1>
+        ? <input
+            ref={inputRef}
+            className="text-3xl text-center py-3 min-w-full" 
+            type='text'
+            value={value}
+            onChange={onChange}
+            onBlur={toggleEditing} />
+        : <h1
+            className="text-3xl text-center py-3 font-bold"
+            onClick={toggleEditing}>
+            {value}
+          </h1>
       }
     </div>
   );
